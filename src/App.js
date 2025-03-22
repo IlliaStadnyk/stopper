09d3c8bc0a9d8e0dc03925b56831068ba7701cb3
Illia Stadnyk <ilya.stadnyk@gmail.com>
2025-03-22 13:50:05 +0100

    Updated project

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
 import Container from "./components/Container/Container";
 import Button from "./components/Button/Button";
 import Clock from "./components/Clock/Clock";
-import {useState} from "react";
+import {useEffect, useState} from "react";
 function App() {
 
     const [isRunning, setIsRunning] = useState(false);
+
+    const [time, setTime] = useState(0);
+
+    useEffect(() => {
+        let timer;
+        if (isRunning === 'reset') {
+            setTime(time => 0);
+        } else {
+            if(isRunning) {
+                timer = setInterval(() => {
+                    setTime(time => time + 10);
+                }, 10);
+            }
+        }
+
+        return () => {
+            if (timer) clearInterval(timer);
+        };
+    }, [isRunning]);
 
     const startClock = () => {
         console.log("Starting Clock...");
@@ -23,7 +42,7 @@ function App() {
 
   return (
     <Container>
-      <Clock isRuning={isRunning}></Clock>
+      <Clock time={time}></Clock>
       <Button onClick={startClock}>START</Button>
       <Button onClick={stopClock}>STOP</Button>
       <Button onClick={resetClock}>RESET</Button>
--- src/components/Clock/Clock.js
+++ src/components/Clock/Clock.js
@@ -1,25 +1,7 @@
 import styles from './Clock.module.scss'
-import {useState, useEffect} from 'react'
 
 const Clock = props => {
-     const [time, setTime] = useState(0);
 
-     useEffect(() => {
-         let timer;
-         if (props.isRuning === 'reset') {
-             setTime(time => 0);
-         } else {
-             if(props.isRuning) {
-                 timer = setInterval(() => {
-                     setTime(time => time + 10);
-                 }, 10);
-             }
-         }
-
-         return () => {
-             if (timer) clearInterval(timer);
-         };
-     }, [props.isRuning]);
 
     const formatTime = (milliseconds) => {
         const hours = Math.floor(milliseconds / 3600000);
@@ -31,6 +13,6 @@ const Clock = props => {
         return `${String(hours).padStart(2, '0')}:${String(minutes).padStart(2, '0')}:${String(seconds).padStart(2, '0')}.${String(ms).padStart(3, '0')}`;
     };
 
-    return <h1 className={styles.clock}>{formatTime(time)}</h1>;
+    return <h1 className={styles.clock}>{formatTime(props.time)}</h1>;
  }
  export default Clock;
